34ab1d90794c46a86268048dcc45d8b7040e194f
Nick Butcher <nickbutcher@google.com>
2020-08-18 10:08:39 +0100

    Add theming codelab.
    Change-Id: Ie05928ba2a152663549e152f29bc13751ab231c5

---
+++ ThemingCodelab/app/src/main/java/com/codelab/theming/ui/start/Home.kt
@@ -0,0 +1,207 @@
+/*
+ * Copyright 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.codelab.theming.ui.start
+
+import androidx.compose.foundation.Icon
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.ScrollableColumn
+import androidx.compose.foundation.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.preferredHeight
+import androidx.compose.foundation.layout.preferredHeightIn
+import androidx.compose.material.Card
+import androidx.compose.material.Divider
+import androidx.compose.material.ListItem
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Palette
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.annotatedString
+import androidx.compose.ui.unit.dp
+import androidx.ui.tooling.preview.Preview
+import com.codelab.theming.R
+import com.codelab.theming.data.Post
+import com.codelab.theming.data.PostRepo
+
+@Composable
+fun Home() {
+    val featured = remember { PostRepo.getFeaturedPost() }
+    val posts = remember { PostRepo.getPosts() }
+    MaterialTheme {
+        Scaffold(
+            topBar = { AppBar() }
+        ) { innerPadding ->
+            ScrollableColumn(contentPadding = innerPadding) {
+                Header(stringResource(R.string.top))
+                FeaturedPost(
+                    post = featured,
+                    modifier = Modifier.padding(16.dp)
+                )
+                Header(stringResource(R.string.popular))
+                posts.forEach { post ->
+                    PostItem(post = post)
+                    Divider(startIndent = 72.dp)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun AppBar() {
+    TopAppBar(
+        navigationIcon = {
+            Icon(Icons.Rounded.Palette, Modifier.padding(horizontal = 12.dp))
+        },
+        title = {
+            Text(text = stringResource(R.string.app_title))
+        },
+        backgroundColor = MaterialTheme.colors.primary
+    )
+}
+
+@Composable
+fun Header(
+    text: String,
+    modifier: Modifier = Modifier
+) {
+    Text(
+        text = text,
+        modifier = modifier
+            .fillMaxWidth()
+            .background(Color.LightGray)
+            .padding(horizontal = 16.dp, vertical = 8.dp)
+    )
+}
+
+@Composable
+fun FeaturedPost(
+    post: Post,
+    modifier: Modifier = Modifier
+) {
+    Card(modifier) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .clickable { /* onClick */ }
+        ) {
+            Image(
+                asset = imageResource(post.imageId),
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .preferredHeightIn(minHeight = 180.dp)
+                    .fillMaxWidth()
+            )
+            Spacer(Modifier.preferredHeight(16.dp))
+
+            val padding = Modifier.padding(horizontal = 16.dp)
+            Text(
+                text = post.title,
+                modifier = padding
+            )
+            Text(
+                text = post.metadata.author.name,
+                modifier = padding
+            )
+            PostMetadata(post, padding)
+            Spacer(Modifier.preferredHeight(16.dp))
+        }
+    }
+}
+
+@Composable
+private fun PostMetadata(
+    post: Post,
+    modifier: Modifier = Modifier
+) {
+    val divider = "  •  "
+    val tagDivider = "  "
+    val text = annotatedString {
+        append(post.metadata.date)
+        append(divider)
+        append(stringResource(R.string.read_time, post.metadata.readTimeMinutes))
+        append(divider)
+        post.tags.forEachIndexed { index, tag ->
+            if (index != 0) {
+                append(tagDivider)
+            }
+            append(" ${tag.toUpperCase()} ")
+        }
+    }
+    Text(
+        text = text,
+        modifier = modifier
+    )
+}
+
+@Composable
+fun PostItem(
+    post: Post,
+    modifier: Modifier = Modifier
+) {
+    ListItem(
+        modifier = modifier.padding(vertical = 8.dp),
+        icon = {
+            Image(
+                asset = imageResource(post.imageThumbId),
+            )
+        },
+        text = {
+            Text(text = post.title)
+        },
+        secondaryText = {
+            PostMetadata(post)
+        },
+        onClick = { /* TODO */ }
+    )
+}
+
+@Preview("Post Item")
+@Composable
+private fun PostItemPreview() {
+    val post = remember { PostRepo.getFeaturedPost() }
+    Surface {
+        PostItem(post = post)
+    }
+}
+
+@Preview("Featured Post")
+@Composable
+private fun FeaturedPostPreview() {
+    val post = remember { PostRepo.getFeaturedPost() }
+    FeaturedPost(post = post)
+}
+
+@Preview("Home")
+@Composable
+private fun HomePreview() {
+    Home()
+}
