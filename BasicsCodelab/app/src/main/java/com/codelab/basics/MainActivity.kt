e768758cb9e9ede61a815e7c4ad6e06146fcf9bb
Yacine Rezgui <yrezgui@google.com>
2021-02-10 14:22:35 +0100

    Update dependencies

--- BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
+++ BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
@@ -38,7 +38,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.setContent
+import androidx.activity.compose.setContent
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.codelab.basics.ui.BasicsCodelabTheme
