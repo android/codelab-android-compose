4bcf962249c1591f91967644c17af6401b6c233d
Yuichi Araki <yaraki@google.com>
2021-02-19 13:15:02 +0900

    [AnimationCodelab] Add a new codelab

---
+++ AnimationCodelab/finished/src/main/java/com/example/android/codelab/animation/ui/Color.kt
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.codelab.animation.ui
+
+import androidx.compose.ui.graphics.Color
+
+val purple100 = Color(0xFFE1BEE7)
+val purple500 = Color(0xFF6200EE)
+val purple700 = Color(0xFF3700B3)
+val teal200 = Color(0xFF03DAC5)
+val green300 = Color(0xFF81C784)
+val green800 = Color(0xFF2E7D32)
+val amber600 = Color(0xFFFFB300)
