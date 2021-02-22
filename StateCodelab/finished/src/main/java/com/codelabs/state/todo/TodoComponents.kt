a81fd23f889611e9097f7de976268f797c99a728
Florina Muntenescu <florinam@google.com>
2021-02-22 11:19:15 +0000

    [State] Beta01 update

--- StateCodelab/finished/src/main/java/com/codelabs/state/todo/TodoComponents.kt
+++ StateCodelab/finished/src/main/java/com/codelabs/state/todo/TodoComponents.kt
@@ -49,10 +49,12 @@ import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
@@ -201,6 +203,7 @@ fun TodoItemInputBackground(
  * @param modifier the modifier for this element
  * @param onImeAction (event) notify caller of [ImeAction.Done] events
  */
+@ExperimentalComposeUiApi
 @Composable
 fun TodoInputText(
     text: String,
@@ -208,6 +211,7 @@ fun TodoInputText(
     modifier: Modifier = Modifier,
     onImeAction: () -> Unit = {}
 ) {
+    val keyboardController = LocalSoftwareKeyboardController.current
     TextField(
         value = text,
         onValueChange = onTextChange,
@@ -216,6 +220,7 @@ fun TodoInputText(
         keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done),
         keyboardActions = KeyboardActions(onDone = {
             onImeAction()
+            keyboardController?.hideSoftwareKeyboard()
         }),
         modifier = modifier
     )
