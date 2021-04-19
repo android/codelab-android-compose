4c8f41756329b835d5a10bfe6fed9d948d94b3f7
Ben Weiss <benweiss@google.com>
2021-04-19 13:36:37 +0100

    Add Navigation Codelab start state
    Change-Id: I8c4eb54b2ae8a1522ff53f6929c3c31ee43b61c1

---
+++ NavigationCodelab/app/src/main/java/com/example/compose/rally/ui/theme/RallyTheme.kt
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.compose.rally.ui.theme
+
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Typography
+import androidx.compose.material.darkColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.compositeOver
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.em
+import androidx.compose.ui.unit.sp
+
+/**
+ * A [MaterialTheme] for Rally.
+ */
+@Composable
+fun RallyTheme(content: @Composable () -> Unit) {
+
+    MaterialTheme(colors = ColorPalette, typography = Typography, content = content)
+}
+
+/**
+ * A theme overlay used for dialogs.
+ */
+@Composable
+fun RallyDialogThemeOverlay(content: @Composable () -> Unit) {
+    // Rally is always dark themed.
+    val dialogColors = darkColors(
+        primary = Color.White,
+        surface = Color.White.copy(alpha = 0.12f).compositeOver(Color.Black),
+        onSurface = Color.White
+    )
+
+    // Copy the current [Typography] and replace some text styles for this theme.
+    val currentTypography = MaterialTheme.typography
+    val dialogTypography = currentTypography.copy(
+        body2 = currentTypography.body1.copy(
+            fontWeight = FontWeight.Normal,
+            fontSize = 20.sp,
+            lineHeight = 28.sp,
+            letterSpacing = 1.sp
+        ),
+        button = currentTypography.button.copy(
+            fontWeight = FontWeight.Bold,
+            letterSpacing = 0.2.em
+        )
+    )
+    MaterialTheme(colors = dialogColors, typography = dialogTypography, content = content)
+}
