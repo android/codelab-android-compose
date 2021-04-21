a06cc4e8abbe86dc2395bad6aa31f293d767c388
Jose Alcérreca <jalc@google.com>
2021-04-21 18:44:19 +0000

    Import fix

--- LayoutsCodelab/app/src/main/java/com/codelab/layouts/Lists.kt
+++ LayoutsCodelab/app/src/main/java/com/codelab/layouts/Lists.kt
@@ -35,7 +35,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.google.accompanist.coil.CoilImage
 import com.google.accompanist.coil.rememberCoilPainter
 import kotlinx.coroutines.launch
 
