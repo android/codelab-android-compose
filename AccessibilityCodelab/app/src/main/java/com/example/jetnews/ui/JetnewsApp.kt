8909babeca52d1c72284b8b0e398d95d1660be5b
Jolanda Verhoef <jolandaverhoef@google.com>
2021-07-19 13:13:57 +0200

    Add Accessibility Codelab
    Change-Id: I3ff5dfb3fc608ce391888771793255bb0e92b38b

---
+++ AccessibilityCodelab/app/src/main/java/com/example/jetnews/ui/JetnewsApp.kt
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.jetnews.ui
+
+import androidx.compose.material.Scaffold
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.graphics.Color
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.jetnews.data.AppContainer
+import com.example.jetnews.ui.theme.JetnewsTheme
+import com.google.accompanist.insets.ProvideWindowInsets
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import kotlinx.coroutines.launch
+
+@Composable
+fun JetnewsApp(
+    appContainer: AppContainer
+) {
+    JetnewsTheme {
+        ProvideWindowInsets {
+            val systemUiController = rememberSystemUiController()
+            SideEffect {
+                systemUiController.setSystemBarsColor(Color.Transparent, darkIcons = false)
+            }
+
+            val navController = rememberNavController()
+            val coroutineScope = rememberCoroutineScope()
+            // This top level scaffold contains the app drawer, which needs to be accessible
+            // from multiple screens. An event to open the drawer is passed down to each
+            // screen that needs it.
+            val scaffoldState = rememberScaffoldState()
+
+            val navBackStackEntry by navController.currentBackStackEntryAsState()
+            val currentRoute = navBackStackEntry?.destination?.route ?: MainDestinations.HOME_ROUTE
+            Scaffold(
+                scaffoldState = scaffoldState,
+                drawerContent = {
+                    AppDrawer(
+                        currentRoute = currentRoute,
+                        navigateToHome = { navController.navigate(MainDestinations.HOME_ROUTE) },
+                        navigateToInterests = { navController.navigate(MainDestinations.INTERESTS_ROUTE) },
+                        closeDrawer = { coroutineScope.launch { scaffoldState.drawerState.close() } }
+                    )
+                }
+            ) {
+                JetnewsNavGraph(
+                    appContainer = appContainer,
+                    navController = navController,
+                    scaffoldState = scaffoldState
+                )
+            }
+        }
+    }
+}
