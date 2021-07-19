e57d3e8014c2612d77214e760757d67ec60c55bb
Jolanda Verhoef <jolandaverhoef@google.com>
2021-07-19 13:13:57 +0200

    Add Accessibility Codelab
    Change-Id: Icd02d0f6fe96950c31b69f3753cca0d4e924e9b7

---
+++ AccessibilityCodelab/app/src/main/java/com/example/jetnews/ui/home/HomeScreen.kt
@@ -0,0 +1,211 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.jetnews.ui.home
+
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Divider
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.ScaffoldState
+import androidx.compose.material.Text
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.jetnews.R
+import com.example.jetnews.data.posts.PostsRepository
+import com.example.jetnews.model.Post
+import com.example.jetnews.ui.components.InsetAwareTopAppBar
+import com.example.jetnews.ui.theme.JetnewsTheme
+import com.google.accompanist.insets.LocalWindowInsets
+import com.google.accompanist.insets.rememberInsetsPaddingValues
+import kotlinx.coroutines.launch
+
+/**
+ * Stateful HomeScreen which manages state using [produceUiState]
+ *
+ * @param postsRepository data source for this screen
+ * @param navigateToArticle (event) request navigation to Article screen
+ * @param openDrawer (event) request opening the app drawer
+ * @param scaffoldState (state) state for the [Scaffold] component on this screen
+ */
+@Composable
+fun HomeScreen(
+    postsRepository: PostsRepository,
+    navigateToArticle: (String) -> Unit,
+    openDrawer: () -> Unit,
+    scaffoldState: ScaffoldState = rememberScaffoldState()
+) {
+    HomeScreen(
+        posts = postsRepository.getPosts(),
+        navigateToArticle = navigateToArticle,
+        openDrawer = openDrawer,
+        scaffoldState = scaffoldState
+    )
+}
+
+/**
+ * Responsible for displaying the Home Screen of this application.
+ *
+ * Stateless composable is not coupled to any specific state management.
+ *
+ * @param posts (state) the data to show on the screen
+ * @param favorites (state) favorite posts
+ * @param onToggleFavorite (event) toggles favorite for a post
+ * @param navigateToArticle (event) request navigation to Article screen
+ * @param openDrawer (event) request opening the app drawer
+ * @param scaffoldState (state) state for the [Scaffold] component on this screen
+ */
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun HomeScreen(
+    posts: List<Post>,
+    navigateToArticle: (String) -> Unit,
+    openDrawer: () -> Unit,
+    scaffoldState: ScaffoldState
+) {
+    val coroutineScope = rememberCoroutineScope()
+    Scaffold(
+        scaffoldState = scaffoldState,
+        topBar = {
+            val title = stringResource(id = R.string.app_name)
+            InsetAwareTopAppBar(
+                title = { Text(text = title) },
+                navigationIcon = {
+                    IconButton(onClick = { coroutineScope.launch { openDrawer() } }) {
+                        Icon(
+                            painter = painterResource(R.drawable.ic_jetnews_logo),
+                            contentDescription = stringResource(R.string.cd_open_navigation_drawer)
+                        )
+                    }
+                }
+            )
+        }
+    ) { innerPadding ->
+        val modifier = Modifier.padding(innerPadding)
+        PostList(posts, navigateToArticle, modifier)
+    }
+}
+
+/**
+ * Display a list of posts.
+ *
+ * When a post is clicked on, [navigateToArticle] will be called to navigate to the detail screen
+ * for that post.
+ *
+ * @param posts (state) the list to display
+ * @param navigateToArticle (event) request navigation to Article screen
+ * @param modifier modifier for the root element
+ */
+@Composable
+private fun PostList(
+    posts: List<Post>,
+    navigateToArticle: (postId: String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val postsHistory = posts.subList(0, 3)
+    val postsPopular = posts.subList(3, 5)
+
+    LazyColumn(
+        modifier = modifier,
+        contentPadding = rememberInsetsPaddingValues(
+            insets = LocalWindowInsets.current.systemBars,
+            applyTop = false
+        )
+    ) {
+        items(postsHistory) { post ->
+            PostCardHistory(post, navigateToArticle)
+            PostListDivider()
+        }
+        item {
+            PostListPopularSection(postsPopular, navigateToArticle)
+        }
+    }
+}
+
+/**
+ * Horizontal scrolling cards for [PostList]
+ *
+ * @param posts (state) to display
+ * @param navigateToArticle (event) request navigation to Article screen
+ */
+@Composable
+private fun PostListPopularSection(
+    posts: List<Post>,
+    navigateToArticle: (String) -> Unit
+) {
+    Column {
+        Text(
+            modifier = Modifier.padding(16.dp),
+            text = stringResource(id = R.string.home_popular_section_title),
+            style = MaterialTheme.typography.subtitle1
+        )
+
+        LazyRow(contentPadding = PaddingValues(end = 16.dp)) {
+            items(posts) { post ->
+                PostCardPopular(
+                    post,
+                    navigateToArticle,
+                    Modifier.padding(start = 16.dp, bottom = 16.dp)
+                )
+            }
+        }
+        PostListDivider()
+    }
+}
+
+/**
+ * Full-width divider with padding for [PostList]
+ */
+@Composable
+private fun PostListDivider() {
+    Divider(
+        modifier = Modifier.padding(horizontal = 14.dp),
+        color = MaterialTheme.colors.onSurface.copy(alpha = 0.08f)
+    )
+}
+
+@Preview("Home screen")
+@Preview("Home screen (dark)", uiMode = UI_MODE_NIGHT_YES)
+@Preview("Home screen (big font)", fontScale = 1.5f)
+@Preview("Home screen (large screen)", device = Devices.PIXEL_C)
+@Composable
+fun PreviewHomeScreen() {
+    JetnewsTheme {
+        HomeScreen(
+            posts = PostsRepository().getPosts(),
+            navigateToArticle = { /*TODO*/ },
+            openDrawer = { /*TODO*/ },
+            scaffoldState = rememberScaffoldState()
+        )
+    }
+}
---
+++ AccessibilityCodelab/app/src/main/java/com/example/jetnews/ui/theme/Type.kt
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.jetnews.ui.theme
+
+import androidx.compose.material.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.example.jetnews.R
+
+private val Montserrat = FontFamily(
+    Font(R.font.montserrat_regular),
+    Font(R.font.montserrat_medium, FontWeight.W500),
+    Font(R.font.montserrat_semibold, FontWeight.W600)
+)
+
+private val Domine = FontFamily(
+    Font(R.font.domine_regular),
+    Font(R.font.domine_bold, FontWeight.Bold)
+)
+
+val JetnewsTypography = Typography(
+    defaultFontFamily = Montserrat,
+    h4 = TextStyle(
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 30.sp,
+        letterSpacing = 0.sp
+    ),
+    h5 = TextStyle(
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 24.sp,
+        letterSpacing = 0.sp
+    ),
+    h6 = TextStyle(
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 20.sp,
+        letterSpacing = 0.sp
+    ),
+    subtitle1 = TextStyle(
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 16.sp,
+        letterSpacing = 0.15.sp
+    ),
+    subtitle2 = TextStyle(
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        letterSpacing = 0.1.sp
+    ),
+    body1 = TextStyle(
+        fontFamily = Domine,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        letterSpacing = 0.5.sp
+    ),
+    body2 = TextStyle(
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        letterSpacing = 0.25.sp
+    ),
+    button = TextStyle(
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 14.sp,
+        letterSpacing = 1.25.sp
+    ),
+    caption = TextStyle(
+        fontWeight = FontWeight.Medium,
+        fontSize = 12.sp,
+        letterSpacing = 0.4.sp
+    ),
+    overline = TextStyle(
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 12.sp,
+        letterSpacing = 1.sp
+    )
+)
