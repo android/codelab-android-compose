58333ee58d26d6b307261716fabba5cc7abd3684
Manuel Vivo <mvivo@google.com>
2021-07-22 10:39:51 +0000

    [AdvancedStateCodelab end] Add project

---
+++ AdvancedStateAndSideEffectsCodelab/app/src/main/java/androidx/compose/samples/crane/base/EditableUserInput.kt
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.compose.samples.crane.base
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.LocalContentColor
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.Saver
+import androidx.compose.runtime.saveable.listSaver
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.samples.crane.ui.captionTextStyle
+import androidx.compose.ui.graphics.SolidColor
+
+@Composable
+fun rememberEditableUserInputState(hint: String): EditableUserInputState =
+    rememberSaveable(hint, saver = EditableUserInputState.Saver) {
+        EditableUserInputState(hint, hint)
+    }
+
+class EditableUserInputState(private val hint: String, initialText: String) {
+
+    var text by mutableStateOf(initialText)
+
+    val isHint: Boolean
+        get() = text == hint
+
+    companion object {
+        val Saver: Saver<EditableUserInputState, *> = listSaver(
+            save = { listOf(it.hint, it.text) },
+            restore = {
+                EditableUserInputState(
+                    hint = it[0],
+                    initialText = it[1],
+                )
+            }
+        )
+    }
+}
+
+@Composable
+fun CraneEditableUserInput(
+    state: EditableUserInputState = rememberEditableUserInputState(""),
+    caption: String? = null,
+    @DrawableRes vectorImageId: Int? = null
+) {
+    CraneBaseUserInput(
+        caption = caption,
+        tintIcon = { !state.isHint },
+        showCaption = { !state.isHint },
+        vectorImageId = vectorImageId
+    ) {
+        BasicTextField(
+            value = state.text,
+            onValueChange = { state.text = it },
+            textStyle = if (state.isHint) {
+                captionTextStyle.copy(color = LocalContentColor.current)
+            } else {
+                MaterialTheme.typography.body1.copy(color = LocalContentColor.current)
+            },
+            cursorBrush = SolidColor(LocalContentColor.current)
+        )
+    }
+}
---
+++ AdvancedStateAndSideEffectsCodelab/app/src/main/java/androidx/compose/samples/crane/home/MainViewModel.kt
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.compose.samples.crane.home
+
+import androidx.compose.samples.crane.data.DestinationsRepository
+import androidx.compose.samples.crane.data.ExploreModel
+import androidx.compose.samples.crane.di.DefaultDispatcher
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+import kotlin.random.Random
+
+const val MAX_PEOPLE = 4
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    private val destinationsRepository: DestinationsRepository,
+    @DefaultDispatcher private val defaultDispatcher: CoroutineDispatcher
+) : ViewModel() {
+
+    val hotels: List<ExploreModel> = destinationsRepository.hotels
+    val restaurants: List<ExploreModel> = destinationsRepository.restaurants
+
+    private val _suggestedDestinations = MutableStateFlow<List<ExploreModel>>(emptyList())
+    val suggestedDestinations: StateFlow<List<ExploreModel>>
+        get() = _suggestedDestinations
+
+    init {
+        _suggestedDestinations.value = destinationsRepository.destinations
+    }
+
+    fun updatePeople(people: Int) {
+        viewModelScope.launch {
+            if (people > MAX_PEOPLE) {
+                _suggestedDestinations.value = emptyList()
+            } else {
+                val newDestinations = withContext(defaultDispatcher) {
+                    destinationsRepository.destinations
+                        .shuffled(Random(people * (1..100).shuffled().first()))
+                }
+                _suggestedDestinations.value = newDestinations
+            }
+        }
+    }
+
+    fun toDestinationChanged(newDestination: String) {
+        viewModelScope.launch {
+            val newDestinations = withContext(defaultDispatcher) {
+                destinationsRepository.destinations
+                    .filter { it.city.nameToDisplay.contains(newDestination) }
+            }
+            _suggestedDestinations.value = newDestinations
+        }
+    }
+}
