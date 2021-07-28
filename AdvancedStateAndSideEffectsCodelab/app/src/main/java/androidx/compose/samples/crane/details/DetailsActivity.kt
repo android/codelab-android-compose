ec80ef1c5e0abd55bd6c4ca58e8dc70152a60834
Chris Banes <chrisbanes@users.noreply.github.com>
2021-07-28 18:48:08 +0100

    [Main] Compose 1.0.0 🎉  (#187)
    * [AdvancedStateCodelab main] Add project
    
    * Improve GH action
    
    * Improve README
    
    * Updating Android Studio version requirement
    
    * [Main] Update to SNAPSHOT 7555628
    
    * Try out activity-compose SNAPSHOT
    
    * Update to Compose 1.0.0
    
    Co-authored-by: Manuel Vivo <mvivo@google.com>
    Co-authored-by: Florina Muntenescu <2998890+florina-muntenescu@users.noreply.github.com>

---
+++ AdvancedStateAndSideEffectsCodelab/app/src/main/java/androidx/compose/samples/crane/details/DetailsActivity.kt
@@ -0,0 +1,219 @@
+/*
+ * Copyright 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.compose.samples.crane.details
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.annotation.VisibleForTesting
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.samples.crane.base.Result
+import androidx.compose.samples.crane.data.ExploreModel
+import androidx.compose.samples.crane.ui.CraneTheme
+import androidx.compose.samples.crane.util.ProvideImageLoader
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.view.WindowCompat
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.google.accompanist.insets.ProvideWindowInsets
+import com.google.accompanist.insets.navigationBarsPadding
+import com.google.accompanist.insets.statusBarsPadding
+import com.google.android.libraries.maps.CameraUpdateFactory
+import com.google.android.libraries.maps.MapView
+import com.google.android.libraries.maps.model.LatLng
+import com.google.maps.android.ktx.addMarker
+import com.google.maps.android.ktx.awaitMap
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+
+internal const val KEY_ARG_DETAILS_CITY_NAME = "KEY_ARG_DETAILS_CITY_NAME"
+
+fun launchDetailsActivity(context: Context, item: ExploreModel) {
+    context.startActivity(createDetailsActivityIntent(context, item))
+}
+
+@VisibleForTesting
+fun createDetailsActivityIntent(context: Context, item: ExploreModel): Intent {
+    val intent = Intent(context, DetailsActivity::class.java)
+    intent.putExtra(KEY_ARG_DETAILS_CITY_NAME, item.city.name)
+    return intent
+}
+
+@AndroidEntryPoint
+class DetailsActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
+        setContent {
+            ProvideWindowInsets {
+                ProvideImageLoader {
+                    CraneTheme {
+                        Surface {
+                            DetailsScreen(
+                                onErrorLoading = { finish() },
+                                modifier = Modifier
+                                    .statusBarsPadding()
+                                    .navigationBarsPadding()
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun DetailsScreen(
+    onErrorLoading: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: DetailsViewModel = viewModel()
+) {
+    // TODO Codelab: produceState step - Show loading screen while fetching city details
+    val cityDetails = remember(viewModel) { viewModel.cityDetails }
+    if (cityDetails is Result.Success<ExploreModel>) {
+        DetailsContent(cityDetails.data, modifier.fillMaxSize())
+    } else {
+        onErrorLoading()
+    }
+}
+
+@Composable
+fun DetailsContent(
+    exploreModel: ExploreModel,
+    modifier: Modifier = Modifier
+) {
+    Column(modifier = modifier, verticalArrangement = Arrangement.Center) {
+        Spacer(Modifier.height(32.dp))
+        Text(
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            text = exploreModel.city.nameToDisplay,
+            style = MaterialTheme.typography.h4,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            text = exploreModel.description,
+            style = MaterialTheme.typography.h6,
+            textAlign = TextAlign.Center
+        )
+        Spacer(Modifier.height(16.dp))
+        CityMapView(exploreModel.city.latitude, exploreModel.city.longitude)
+    }
+}
+
+@Composable
+private fun CityMapView(latitude: String, longitude: String) {
+    // The MapView lifecycle is handled by this composable. As the MapView also needs to be updated
+    // with input from Compose UI, those updates are encapsulated into the MapViewContainer
+    // composable. In this way, when an update to the MapView happens, this composable won't
+    // recompose and the MapView won't need to be recreated.
+    val mapView = rememberMapViewWithLifecycle()
+    MapViewContainer(mapView, latitude, longitude)
+}
+
+@Composable
+private fun MapViewContainer(
+    map: MapView,
+    latitude: String,
+    longitude: String
+) {
+    val cameraPosition = remember(latitude, longitude) {
+        LatLng(latitude.toDouble(), longitude.toDouble())
+    }
+
+    LaunchedEffect(map) {
+        val googleMap = map.awaitMap()
+        googleMap.addMarker { position(cameraPosition) }
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(cameraPosition))
+    }
+
+    var zoom by rememberSaveable(map) { mutableStateOf(InitialZoom) }
+    ZoomControls(zoom) {
+        zoom = it.coerceIn(MinZoom, MaxZoom)
+    }
+
+    val coroutineScope = rememberCoroutineScope()
+    AndroidView({ map }) { mapView ->
+        // Reading zoom so that AndroidView recomposes when it changes. The getMapAsync lambda
+        // is stored for later, Compose doesn't recognize state reads
+        val mapZoom = zoom
+        coroutineScope.launch {
+            val googleMap = mapView.awaitMap()
+            googleMap.setZoom(mapZoom)
+            // Move camera to the same place to trigger the zoom update
+            googleMap.moveCamera(CameraUpdateFactory.newLatLng(cameraPosition))
+        }
+    }
+}
+
+@Composable
+private fun ZoomControls(
+    zoom: Float,
+    onZoomChanged: (Float) -> Unit
+) {
+    Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+        ZoomButton("-", onClick = { onZoomChanged(zoom * 0.8f) })
+        ZoomButton("+", onClick = { onZoomChanged(zoom * 1.2f) })
+    }
+}
+
+@Composable
+private fun ZoomButton(text: String, onClick: () -> Unit) {
+    Button(
+        modifier = Modifier.padding(8.dp),
+        colors = ButtonDefaults.buttonColors(
+            backgroundColor = MaterialTheme.colors.onPrimary,
+            contentColor = MaterialTheme.colors.primary
+        ),
+        onClick = onClick
+    ) {
+        Text(text = text, style = MaterialTheme.typography.h5)
+    }
+}
+
+private const val InitialZoom = 5f
+const val MinZoom = 2f
+const val MaxZoom = 20f
---
+++ AdvancedStateAndSideEffectsCodelab/app/src/main/java/androidx/compose/samples/crane/details/MapViewUtils.kt
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.compose.samples.crane.details
+
+import android.os.Bundle
+import androidx.annotation.FloatRange
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.samples.crane.R
+import androidx.compose.ui.platform.LocalContext
+import com.google.android.libraries.maps.GoogleMap
+import com.google.android.libraries.maps.MapView
+
+/**
+ * Remembers a MapView and gives it the lifecycle of the current LifecycleOwner
+ */
+@Composable
+fun rememberMapViewWithLifecycle(): MapView {
+    val context = LocalContext.current
+    // TODO Codelab: DisposableEffect step. Make MapView follow the lifecycle
+    return remember {
+        MapView(context).apply {
+            id = R.id.map
+            onCreate(Bundle())
+        }
+    }
+}
+
+fun GoogleMap.setZoom(
+    @FloatRange(from = MinZoom.toDouble(), to = MaxZoom.toDouble()) zoom: Float
+) {
+    resetMinMaxZoomPreference()
+    setMinZoomPreference(zoom)
+    setMaxZoomPreference(zoom)
+}
