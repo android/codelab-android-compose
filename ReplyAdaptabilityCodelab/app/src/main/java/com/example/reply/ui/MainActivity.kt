6eca8d7b58a4a6b1b8e272e519b190b21d3a7afe
Gurupreet Singh <gurusingh@google.com>
2022-05-03 15:31:13 +0530

    added multisize previews

--- ReplyAdaptabilityCodelab/app/src/main/java/com/example/reply/ui/MainActivity.kt
+++ ReplyAdaptabilityCodelab/app/src/main/java/com/example/reply/ui/MainActivity.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.reply.data.EmailsRepositoryImpl
+import com.example.reply.data.local.LocalEmailsDataProvider
 import com.example.reply.ui.theme.ReplyTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,6 +32,28 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun ReplyAppPreview() {
     ReplyTheme {
-        ReplyApp(replyHomeUIState = ReplyHomeUIState())
+        ReplyApp(replyHomeUIState = ReplyHomeUIState(
+            emails = LocalEmailsDataProvider.allEmails
+        ))
+    }
+}
+
+@Preview(showBackground = true, widthDp = 700)
+@Composable
+fun ReplyAppPreviewTablet() {
+    ReplyTheme {
+        ReplyApp(replyHomeUIState = ReplyHomeUIState(
+            emails = LocalEmailsDataProvider.allEmails
+        ))
+    }
+}
+
+@Preview(showBackground = true, widthDp = 1000)
+@Composable
+fun ReplyAppPreviewDesktop() {
+    ReplyTheme {
+        ReplyApp(replyHomeUIState = ReplyHomeUIState(
+            emails = LocalEmailsDataProvider.allEmails
+        ))
     }
 }
