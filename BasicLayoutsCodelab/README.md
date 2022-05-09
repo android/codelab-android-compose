cd868ee27bbb8b510bd27c9807684e465a565ab3
Jolanda Verhoef <JolandaVerhoef@users.noreply.github.com>
2022-05-09 15:37:46 +0200

    Basics Layout Codelab end code
    Change-Id: I15fd841f56d9a0dde867c8d61abfc2d8178a2e51

---
+++ BasicLayoutsCodelab/README.md
@@ -0,0 +1,37 @@
+# Basic Layouts in Compose Codelab
+
+This folder contains the source code for
+the [Basic Layouts in Compose Codelab](https://developer.android.com/codelabs/jetpack-compose-layouts)
+
+## License
+
+```
+Copyright 2022 The Android Open Source Project
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+```
+
+## Image asset attributions
+
+[fc1_short_mantras.jpg](https://www.pexels.com/photo/body-of-water-view-1825206/) - Elizaveta Kozorezova
+[fc2_nature_meditations.jpg](https://www.pexels.com/photo/photo-of-green-leaves-3571551/) - Nothing Ahead
+[fc3_stress_and_anxiety.jpg](https://www.pexels.com/photo/aerial-view-of-body-of-water-1557238/) - Jim
+[fc4_self_massage.jpg](https://www.pexels.com/photo/photography-of-stones-1029604/) - Scott Webb
+[fc5_overwhelmed.jpg](https://www.pexels.com/photo/white-clouds-3560044/) - Ruvim
+[fc6_nightly_wind_down.jpg](https://www.pexels.com/photo/time-lapse-photo-of-stars-on-night-924824/) - Jakub Novacek
+[ab1_inversions.jpg](https://www.pexels.com/photo/low-angle-view-of-woman-relaxing-on-beach-against-blue-sky-317157/) - Chevanon Photography
+[ab2_quick_yoga.jpg](https://www.pexels.com/photo/photo-of-woman-stretching-her-body-1812964/) - Agung Pandit Wiguna
+[ab3_stretching.jpg](https://www.pexels.com/photo/photo-of-women-stretching-together-4056723/) - Cliff Booth
+[ab4_tabata.jpg](https://www.pexels.com/photo/fashion-man-people-art-4662438/) - Elly Fairytale
+[ab5_hiit.jpg](https://www.pexels.com/photo/man-wearing-white-pants-under-blue-sky-999309/) - The Lazy Artist Gallery
+[ab6_pre_natal_yoga.jpg](https://www.pexels.com/photo/woman-doing-yoga-396133/) - freestocks.org
---
+++ BasicLayoutsCodelab/app/src/main/java/com/codelab/basiclayouts/ui/theme/Type.kt
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.codelab.basiclayouts.ui.theme
+
+import androidx.compose.material.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.codelab.basiclayouts.R
+
+private val fontFamilyKulim = FontFamily(
+    listOf(
+        Font(
+            resId = R.font.kulim_park_regular
+        ),
+        Font(
+            resId = R.font.kulim_park_light,
+            weight = FontWeight.Light
+        )
+    )
+)
+
+private val fontFamilyLato = FontFamily(
+    listOf(
+        Font(
+            resId = R.font.lato_regular
+        ),
+        Font(
+            resId = R.font.lato_bold,
+            weight = FontWeight.Bold
+        )
+    )
+)
+
+val typography = Typography(
+    defaultFontFamily = fontFamilyLato,
+    h1 = TextStyle(
+        fontFamily = fontFamilyKulim,
+        fontWeight = FontWeight.Light,
+        fontSize = 28.sp,
+        letterSpacing = (1.15).sp
+    ),
+    h2 = TextStyle(
+        fontFamily = fontFamilyKulim,
+        fontSize = 15.sp,
+        letterSpacing = (1.15).sp
+    ),
+    h3 = TextStyle(
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp,
+        letterSpacing = 0.sp
+    ),
+    body1 = TextStyle(
+        fontSize = 14.sp,
+        letterSpacing = 0.sp
+    ),
+    button = TextStyle(
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp,
+        letterSpacing = (1.15).sp
+    ),
+    caption = TextStyle(
+        fontFamily = fontFamilyKulim,
+        fontSize = 12.sp,
+        letterSpacing = (1.15).sp
+    ),
+)
