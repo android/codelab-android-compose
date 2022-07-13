c3e8b8aa5f9cf8f0eceb429578bfd6455eab040a
Kessirin296 <builder@Kessirin296s-MacBook-Pro.local>
2022-07-13 12:27:00 +0700

    Add composable for header of UI

--- MigrationCodelab/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
+++ MigrationCodelab/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
@@ -16,13 +16,42 @@
 
 package com.google.samples.apps.sunflower.plantdetail
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.google.samples.apps.sunflower.R
 
 @Composable
 fun PlantDetailDescription() {
     Surface {
         Text("Hello Compose")
+    }
+}
+
+@Composable
+fun PlantName(name: String) {
+    Text(
+        text = name,
+        modifier = Modifier
+            .padding(horizontal = dimensionResource(id = R.dimen.margin_small))
+            .fillMaxWidth()
+            .wrapContentWidth(Alignment.CenterHorizontally),
+        style = MaterialTheme.typography.h5
+    )
+}
+
+@Preview
+@Composable
+fun PlantNamePreview() {
+    MaterialTheme {
+        PlantName(name = "Avocado")
     }
 }
