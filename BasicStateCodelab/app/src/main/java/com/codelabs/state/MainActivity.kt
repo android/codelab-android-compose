fe784ba56eafbf8501618a779f6f1c3cf1f3591a
Florina Muntenescu <florinam@google.com>
2023-02-01 17:21:36 +0000

    [Basic State] Migrating to use Material3.
    Using the starting code from New Activity template

--- BasicStateCodelab/app/src/main/java/com/codelabs/state/MainActivity.kt
+++ BasicStateCodelab/app/src/main/java/com/codelabs/state/MainActivity.kt
@@ -19,8 +19,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.codelabs.state.ui.theme.BasicStateCodelabTheme
 
@@ -32,7 +32,7 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    color = MaterialTheme.colorScheme.background
                 ) {
                     WellnessScreen()
                 }
--- BasicStateCodelab/app/src/main/java/com/codelabs/state/WellnessTaskItem.kt
+++ BasicStateCodelab/app/src/main/java/com/codelabs/state/WellnessTaskItem.kt
@@ -17,12 +17,12 @@ package com.codelabs.state
 
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Checkbox
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
