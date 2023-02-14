6157a4da6d136d6d46628e15f18e66cdbbe46bc9
Gurupreet Singh <gurusingh@google.com>
2023-02-14 15:20:25 +0100

    removed cache tracked files

--- ThemingCodelab/app/build/generated/source/buildConfig/debug/com/example/reply/BuildConfig.java
+++ ThemingCodelab/app/build/generated/source/buildConfig/debug/com/example/reply/BuildConfig.java
@@ -4,9 +4,9 @@
 package com.example.reply;
 
 public final class BuildConfig {
-    public static final boolean DEBUG = Boolean.parseBoolean("true");
-    public static final String APPLICATION_ID = "com.example.jetnews";
-    public static final String BUILD_TYPE = "debug";
-    public static final int VERSION_CODE = 1;
-    public static final String VERSION_NAME = "1.0";
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.jetnews";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
 }
