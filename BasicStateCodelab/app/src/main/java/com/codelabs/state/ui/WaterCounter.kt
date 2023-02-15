8aa2c9c56570b44c422ff17de0dd549cd75ab3f0
Máté Iszlai <iszimate95@gmail.com>
2023-02-15 14:56:32 +0100

    Implement counter

--- BasicStateCodelab/app/src/main/java/com/codelabs/state/ui/WaterCounter.kt
+++ BasicStateCodelab/app/src/main/java/com/codelabs/state/ui/WaterCounter.kt
@@ -4,7 +4,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -13,7 +13,7 @@ import com.codelabs.state.R
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier.padding(16.dp)) {
-        var count = 0
+        var count by remember { mutableStateOf(0) }
 
         Text(
             text = String.format(stringResource(id = R.string.water_counter_text), count),
