99f4c9a08af9c638d7b95c484d05433f126a1767
Máté Iszlai <iszimate95@gmail.com>
2023-02-15 14:50:41 +0100

    Add button to WaterCounter

--- BasicStateCodelab/app/src/main/java/com/codelabs/state/ui/WaterCounter.kt
+++ BasicStateCodelab/app/src/main/java/com/codelabs/state/ui/WaterCounter.kt
@@ -1,30 +1,29 @@
 package com.codelabs.state.ui
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.codelabs.state.R
-import com.codelabs.state.ui.theme.BasicStateCodelabTheme
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
-    val count = 0
-    Text(
-        text = String.format(stringResource(id = R.string.water_counter_text), count),
-        modifier = modifier.padding(16.dp)
-    )
-}
+    Column(modifier.padding(16.dp)) {
+        var count = 0
 
-@Preview(
-    showBackground = true
-)
-@Composable
-fun WaterCounterPreview() {
-    BasicStateCodelabTheme {
-        WaterCounter()
+        Text(
+            text = String.format(stringResource(id = R.string.water_counter_text), count),
+            modifier = modifier.padding(16.dp)
+        )
+        Button(
+            onClick = { count++ },
+            modifier = Modifier.padding(top = 8.dp)
+        ) {
+            Text(text = stringResource(id = R.string.counter_btn_text))
+        }
     }
 }
