1ffa75ac04aca5dbf56044f2ff16eff2d704a60e
Máté Iszlai <iszimate95@gmail.com>
2023-02-15 15:30:28 +0100

    Create WellnessTaskItem.kt

---
+++ BasicStateCodelab/app/src/main/java/com/codelabs/state/ui/WellnessTaskItem.kt
@@ -0,0 +1,40 @@
+package com.codelabs.state.ui
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.codelabs.state.R
+
+@Composable
+fun WellnessTaskItem(
+    taskName: String,
+    onClose: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = taskName,
+            modifier = Modifier
+                .weight(1f)
+                .padding(start = 16.dp)
+        )
+        IconButton(onClick = onClose) {
+            Icon(
+                imageVector = Icons.Filled.Close,
+                contentDescription = stringResource(id = R.string.close_btn_text)
+            )
+        }
+    }
+}
