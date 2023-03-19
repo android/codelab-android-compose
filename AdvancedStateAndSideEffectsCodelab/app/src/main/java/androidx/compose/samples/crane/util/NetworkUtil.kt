e7dea3ad2b1b6fd344d1af997227366f3bcd2521
Ale Stamato <stamato@google.com>
2023-03-19 16:56:11 +0000

    Fixes use landing screen (#349)

--- AdvancedStateAndSideEffectsCodelab/app/src/main/java/androidx/compose/samples/crane/util/NetworkUtil.kt
+++ AdvancedStateAndSideEffectsCodelab/app/src/main/java/androidx/compose/samples/crane/util/NetworkUtil.kt
@@ -16,17 +16,13 @@
 
 package androidx.compose.samples.crane.util
 
-import coil.annotation.ExperimentalCoilApi
 import coil.intercept.Interceptor
 import coil.request.ImageResult
-import coil.size.Size
-import okhttp3.HttpUrl
 import okhttp3.HttpUrl.Companion.toHttpUrl
 
 /**
  * A Coil [Interceptor] which appends query params to Unsplash urls to request sized images.
  */
-@OptIn(ExperimentalCoilApi::class)
 object UnsplashSizingInterceptor : Interceptor {
     override suspend fun intercept(chain: Interceptor.Chain): ImageResult {
         val data = chain.request.data
