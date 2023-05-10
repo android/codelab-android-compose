deb6c41c5450e44b640b2cf90ba1356b006a160f
Florina Muntenescu <2998890+florina-muntenescu@users.noreply.github.com>
2023-05-10 10:54:31 +0100

    [Basic State] Migrating to use Material3. (#333)
    Using the starting code from New Activity template

--- BasicStateCodelab/app/src/main/java/com/codelabs/state/WaterCounter.kt
+++ BasicStateCodelab/app/src/main/java/com/codelabs/state/WaterCounter.kt
@@ -17,8 +17,8 @@ package com.codelabs.state
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
-import androidx.compose.material.Text
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
