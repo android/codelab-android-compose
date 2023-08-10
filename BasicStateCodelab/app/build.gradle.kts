f80f51b16e7a423d5aa2ded912967786f3a75b33
Simona Stojanovic <anomis@google.com>
2023-08-10 13:46:12 +0100

    Update Compose BOM

--- BasicStateCodelab/app/build.gradle.kts
+++ BasicStateCodelab/app/build.gradle.kts
@@ -70,7 +70,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
     implementation("androidx.activity:activity-compose:1.7.2")
-    implementation(platform("androidx.compose:compose-bom:2023.06.01"))
+    implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
