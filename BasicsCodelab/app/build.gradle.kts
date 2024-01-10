bb061c27ed943bed94a8a6cf01d5bdb77cacaf14
Meghan <magicalmeghan@google.com>
2024-01-10 17:32:30 -0500

    update tools and kotlin compiler

--- BasicsCodelab/app/build.gradle.kts
+++ BasicsCodelab/app/build.gradle.kts
@@ -56,7 +56,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.8"
+        kotlinCompilerExtensionVersion = "1.5.7"
     }
     packaging {
         resources {
