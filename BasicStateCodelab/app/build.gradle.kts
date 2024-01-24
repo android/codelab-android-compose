6d9eb1db385918a8e1806cb021d88674b08ffad7
Ben Trengrove <bentrengrove@google.com>
2024-01-25 08:19:20 +1100

    Update compiler version

--- BasicStateCodelab/app/build.gradle.kts
+++ BasicStateCodelab/app/build.gradle.kts
@@ -56,7 +56,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.7"
+        kotlinCompilerExtensionVersion = "1.5.8"
     }
     packaging {
         resources {
