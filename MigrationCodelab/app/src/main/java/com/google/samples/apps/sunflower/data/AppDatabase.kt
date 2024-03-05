1582e1e3fa983a81a1ed9fec12524529ea800d3c
MarkADom <marco.a.dom78@gmail.com>
2024-03-05 21:15:10 +0000

    view to composable

--- MigrationCodelab/app/src/main/java/com/google/samples/apps/sunflower/data/AppDatabase.kt
+++ MigrationCodelab/app/src/main/java/com/google/samples/apps/sunflower/data/AppDatabase.kt
@@ -47,8 +47,6 @@ abstract class AppDatabase : RoomDatabase() {
             }
         }
 
-        // Create and pre-populate the database. See this article for more details:
-        // https://medium.com/google-developers/7-pro-tips-for-room-fbadea4bfbd1#4785
         private fun buildDatabase(context: Context): AppDatabase {
             return Room.databaseBuilder(context, AppDatabase::class.java, DATABASE_NAME)
                 .addCallback(object : RoomDatabase.Callback() {
