c36e0173f2a1f84df4f257db6a8fa09e60a5364d
Jonathan Koren <jdkoren@google.com>
2024-05-08 13:52:24 -0700

    Move plugins to version catalog

--- AdaptiveUiCodelab/app/build.gradle.kts
+++ AdaptiveUiCodelab/app/build.gradle.kts
@@ -15,8 +15,8 @@
  */
 
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
 }
 
 android {
--- AdaptiveUiCodelab/build.gradle.kts
+++ AdaptiveUiCodelab/build.gradle.kts
@@ -19,15 +19,9 @@ buildscript {
         google()
         mavenCentral()
     }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.4.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.23")
-    }
 }
 
-subprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
 }
