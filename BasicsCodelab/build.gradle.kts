12792384f16c4e160228ef2c6327fddb928de193
Tomáš Mlynarič <mlykotom@google.com>
2024-06-14 09:31:42 +0200

    Use Compose compiler to fix kotlin 2.0 issue

--- BasicsCodelab/build.gradle.kts
+++ BasicsCodelab/build.gradle.kts
@@ -18,4 +18,5 @@
 plugins {
     id("com.android.application") version "8.4.0" apply false
     id("org.jetbrains.kotlin.android") version "2.0.0" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.0" apply false
 }
