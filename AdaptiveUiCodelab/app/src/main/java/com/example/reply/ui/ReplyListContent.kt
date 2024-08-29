3073372733f4ae5816bc0d3eb8ecfba397fde0c0
Ash Nohe <ashnohe@google.com>
2024-08-29 16:25:32 -0700

    remove excess padding at bottom of last list item due to handling task bar inset

--- AdaptiveUiCodelab/app/src/main/java/com/example/reply/ui/ReplyListContent.kt
+++ AdaptiveUiCodelab/app/src/main/java/com/example/reply/ui/ReplyListContent.kt
@@ -20,12 +20,14 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeContent
 import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
@@ -46,9 +48,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import com.example.reply.R
 import com.example.reply.data.Email
@@ -59,9 +65,12 @@ fun ReplyListPane(
     onEmailClick: (Email) -> Unit,
     modifier: Modifier = Modifier
 ) {
+    val layoutDirection = LocalLayoutDirection.current
+
     LazyColumn(
         modifier = modifier.fillMaxWidth(),
         contentPadding = WindowInsets.safeDrawing.asPaddingValues()
+            .copy(layoutDirection, bottom = 0.dp)
     ) {
         item {
             ReplySearchBar(modifier = Modifier.fillMaxWidth())
@@ -301,3 +310,17 @@ fun ReplySearchBar(modifier: Modifier = Modifier) {
         )
     }
 }
+
+private fun PaddingValues.copy(
+    layoutDirection: LayoutDirection,
+    start: Dp? = null,
+    top: Dp? = null,
+    end: Dp? = null,
+    bottom: Dp? = null,
+) = PaddingValues(
+    start = start ?: calculateStartPadding(layoutDirection),
+    top = top ?: calculateTopPadding(),
+    end = end ?: calculateEndPadding(layoutDirection),
+    bottom = bottom ?: calculateBottomPadding(),
+)
+
