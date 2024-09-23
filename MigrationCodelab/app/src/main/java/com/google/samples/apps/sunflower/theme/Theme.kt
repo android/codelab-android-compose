ab5eecfa7321de82434960c42d6714d74812db36
Simona Milanovic <anomis@google.com>
2024-09-23 18:23:49 +0100

    Migrate Migration codelab main to M3

--- MigrationCodelab/app/src/main/java/com/google/samples/apps/sunflower/theme/Theme.kt
+++ MigrationCodelab/app/src/main/java/com/google/samples/apps/sunflower/theme/Theme.kt
@@ -16,7 +16,7 @@
 
 package com.google.samples.apps.sunflower.theme
 
-import androidx.compose.material.MaterialTheme
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 
 @Composable
