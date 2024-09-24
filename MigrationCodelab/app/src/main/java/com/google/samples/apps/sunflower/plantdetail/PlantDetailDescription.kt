e15b941b4d1d7aac91d10b41bb568471a41f9960
Simona <35065668+simona-anomis@users.noreply.github.com>
2024-09-24 12:30:28 +0100

    Migrate Migration codelab main to M3 (#492)

--- MigrationCodelab/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
+++ MigrationCodelab/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
@@ -16,8 +16,8 @@
 
 package com.google.samples.apps.sunflower.plantdetail
 
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
 @Composable
