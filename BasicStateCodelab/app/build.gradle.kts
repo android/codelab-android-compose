ef0eedfa36fb6a3b71628296a67ceef553f3060e
Rebecca Franks <franks.rebecca@gmail.com>
2026-06-22 14:53:19 +0100

    Update to java 17

--- BasicStateCodelab/app/build.gradle.kts
+++ BasicStateCodelab/app/build.gradle.kts
@@ -49,13 +49,13 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility= JavaVersion.VERSION_17
+        targetCompatibility= JavaVersion.VERSION_17
     }
 
     kotlin {
         compilerOptions {
-            jvmTarget = JvmTarget.JVM_1_8
+            jvmTarget = JvmTarget.JVM_17
         }
     }
     buildFeatures {
